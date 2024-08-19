65427ec15d2b6c956d52c6d320a7c13b3bc27d06
anurag <anuragdw0710@gmail.com>
2024-08-20 00:02:05 +0530

    added some css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import GraphInput from "./components/GraphInput";
 import GraphComponent from "./components/GraphComponent";
-
+import "./App.css";
 function App() {
   const [graphData, setGraphData] = useState(null);
 
@@ -12,9 +12,11 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Graph Drawing App</h1>
-      <GraphInput onSubmit={handleGraphSubmit} />
-      {graphData && <GraphComponent graphData={graphData} />}
+      <h1 className="main-heading">Graph Drawing App</h1>
+      <div className="dashboard">
+        <GraphInput onSubmit={handleGraphSubmit} />
+        {graphData && <GraphComponent graphData={graphData} />}
+      </div>
     </div>
   );
 }
--- src/components/GraphInput.jsx
+++ src/components/GraphInput.jsx
@@ -36,16 +36,24 @@ const GraphInput = ({ onSubmit }) => {
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
+    <form onSubmit={handleFormSubmit} className="form-box">
       <textarea
         rows="10"
         cols="30"
+        className="textarea"
         value={input}
         onChange={handleInputChange}
-        placeholder="Enter graph data here"
+        placeholder="Enter graph data here as
+n m
+u1 v1
+u2 v2
+u3 v3 
+u4 v4
+..."
       />
-      <br />
-      <button type="submit">Draw Graph</button>
+      <button type="submit" className="button">
+        Draw Graph
+      </button>
       {error && <div style={{ color: "red" }}>{error}</div>}
     </form>
   );
